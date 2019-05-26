b7de903d901e7cea576b213d9caf065e73281c0a
gymfox <solod.andrew93@gmail.com>
2019-05-26 13:17:33 +0300

    Decided Viral Advertising. Removed redundant variable.

--- src/com/gymfox/viral_advertising/Solution.java
+++ src/com/gymfox/viral_advertising/Solution.java
@@ -4,15 +4,13 @@ public class Solution {
     private static final int HALF = 2;
 
     static int viralAdvertising(int n) {
-        int[] days = new int[n];
         int shared = 5;
         int cumulative = 0;
 
-        for (int i = 0; i < days.length; i++) {
-            days[i] = shared / HALF;
-            shared = days[i] * 3;
+        for (int i = 0; i < n; i++) {
+            shared = shared / HALF * 3;
 
-            cumulative += days[i];
+            cumulative += shared;
         }
 
         return cumulative;

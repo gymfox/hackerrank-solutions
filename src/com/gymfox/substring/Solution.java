90712f8eb40a8dbdf67e69a4e18af99abc0fd526
Solod Andrew <solod.andrew93@gmail.com>
2021-07-01 07:08:11 +0300

    Added new solutions

--- src/com/gymfox/substring/Solution.java
+++ src/com/gymfox/substring/Solution.java
@@ -6,10 +6,27 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String S = in.next();
-        int start = in.nextInt();
-        int end = in.nextInt();
+        int k = in.nextInt();
 
-        System.out.println(S.substring(start, end));
+        System.out.println(getSmallestAndLargest(S, k));
+    }
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String sequence = s.substring(0, k);
+        String smallest = sequence;
+        String largest = sequence;
+
+        for (int i = 1; i <= (s.length() - k); i++) {
+            sequence = s.substring(i, (i + k));
+            if (sequence.compareTo(smallest) < 0) {
+                smallest = sequence;
+            }
+            if (sequence.compareTo(largest) > 0) {
+                largest = sequence;
+            }
+        }
+
+        return smallest + "\n" + largest;
     }
 }
 // Helloworld
